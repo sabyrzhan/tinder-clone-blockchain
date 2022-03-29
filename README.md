927d16ca0c83dbd6e950e78d8ec6c509e59991b2
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2022-03-29 23:23:21 +0300

    Finished project by implementing minting and uploading to OpenSea.

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
 ## Tinder clone in blockchain using Next.js
+Tinder clone implementation from https://www.youtube.com/watch?v=4YjUaQKE6-I using Blockchain apps. 
+
 #Requirements
 1. Nodejs (`brew install node`)
 2. Yarn (`npm install -g yarn`)
 3. NextJS (`npx create-next-app@latest .` in project folder)
+4. Blockchain accounts:
+   1. Moralis (https://moralis.io)
+   2. Sanity (https://www.sanity.io)
+   3. Alchemy (https://www.alchemy.com)
+   4. Metamask (https://metamask.io)
+   5. OpenSea (https://testnets.opensea.io)
+5. https://vanity-eth.tk/ -- fake eth address generator
+6. https://unsplash.com -- for test images
 
 # Running locally
 1. Do `npm install`
---
+++ pages/api/mintMatchNft.js
@@ -0,0 +1,54 @@
+import { Moralis } from "moralis/node";
+import { ethers } from "ethers";
+import { TINDER_ADDRESS, TINDER_ABI } from '../../lib/constants';
+
+const mintMatchNft = async (req,res) => {
+    await Moralis.start({
+        serverUrl: process.env.MORALIS_SERVER_URL,
+        appId: process.env.MORALIS_APP_ID,
+        masterKey: process.env.MORALIS_MASTER_KEY
+    })
+
+    const metadata = {
+        name: `${req.body.names[0]} & ${req.body.names[1]}`,
+        description: `${req.body.names[0].split(' ')[0]} & ${req.body.names[1].split(' ')[0]} just matched!`,
+        image: `ipfs://QmY4tKpDGzVHzaSkQc5gzVMCMNoznZqaX15DXkyL2bPp8Z`
+    }
+
+    const base64 = Buffer.from(JSON.stringify(metadata)).toString('base64')
+    const metadataFile = new Moralis.File('file.json', { base64: base64 })
+
+    await metadataFile.saveIPFS({ useMasterKey: true })
+
+    const metadataURI = metadataFile.ipfs()
+    const provider = ethers.getDefaultProvider(process.env.ALCHEMY_API_URL, {
+        chainId: 4,
+        name: 'rinkeby'
+    })
+    const walletWithProvider = new ethers.Wallet(
+        process.env.WALLET_PRIVATE_KEY,
+        provider
+    )
+
+    const contract = new ethers.Contract(
+        TINDER_ADDRESS,
+        TINDER_ABI,
+        walletWithProvider
+    )
+
+    const tx = await contract.mintNFT(
+        req.body.walletAddresses[0],
+        req.body.walletAddresses[1],
+        metadataURI
+    )
+
+    console.log('Transaction', tx)
+
+    const txReceipt = await tx.wait()
+
+    console.log('Transaction receipt', txReceipt)
+
+    res.status(200).send({ message: 'success', data: { tx: tx, txReceipt: txReceipt } })
+}
+
+export default mintMatchNft
